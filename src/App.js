a7d80ff2824e9e262b890d63cb460bde85b47f1a
MKA1500 <maciejkabsch@gmail.com>
2017-12-13 23:32:11 +0100

    new beginnings

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+              <div className="container">
+                <button type="button" class="btn btn-lg btn-primary">Primary</button>
+              </div>
+      </div>
+    );
+  }
+}
+
+export default App;
