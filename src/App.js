5b227d5eccd03067ad3c3b4e5af3a330fec37dc5
MKA1500 <maciejkabsch@gmail.com>
2018-04-24 22:53:55 +0200

    redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React, { Component } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import logo from './logo.svg';
 import './App.css';
 
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'ECMAScript 6. Przewodnik po nowym standardzie języka' },
+    { title: 'JavaScript. Wzorce' },
+    { title: 'JavaScript. Asynchroniczność i wydajność' },
+    { title: 'JavaScript. Zakresy i domknięcia' }
+  ];
+}
