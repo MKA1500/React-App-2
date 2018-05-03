9cf0f0344d809fb000f8ad4ee98349b3dbe1f0e1
MKA1500 <maciejkabsch@gmail.com>
2018-05-03 21:20:16 +0200

    figuring out redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 
+import BookList from './containers/book_list';
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,9 +15,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BookList />
       </div>
     );
   }
--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList(){
-    return this.props.books.map((book) => {
+    return this.props.books.map(book => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}
+        <li
+          key={book.title}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
@@ -17,3 +22,13 @@ export default class BookList extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  // inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
